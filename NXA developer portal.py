f3f72f21cbf852425f55b5a5d1aadd804dd5cfd9
Notxnorand73 <notxnor33415@outlook.com>
2026-02-20 22:43:09 -0600

    Add NXA Developer Portal script

---
+++ NXA developer portal.py
@@ -0,0 +1,55 @@
+import os
+import webbrowser
+import nxaconsole
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+header = "=" * 44
+print(header)
+print("|| NXA Developer Portal || Version: 1.0.0")
+print(header)
+print("|| User:")
+print("|| Role:")
+print(header)
+print("|| Enter your data")
+print(header)
+
+isLoggedIn = False
+
+while not isLoggedIn:
+    username = input("|| Username: ")
+    password = input("|| Password: ")
+    print(header)
+
+    if len(username) == 0 or len(password) == 0:
+        print("Username and password cannot be empty")
+    elif username == "admin" and password == "admin":
+        clear_screen()
+        print("Admin login successful")
+        isLoggedIn = True
+    else:
+        clear_screen()
+        print("Login successful")
+        isLoggedIn = True
+while True:
+    print(header)
+    print("|| Welcome to the NXA Developer Portal")
+    print(header)
+    print("|| Please select an option:")
+    print("|| 1. NXA Developer Console")
+    print("|| 2. NXA GitHub Repository")
+    print("|| 3. Exit")
+    print(header)
+    option = input("|| Enter your choice: ")
+    if option == "1":
+        print("Opening NXA Developer Console...")
+        nxaconsole.start_console()
+    elif option == "2":
+        print("Opening NXA GitHub Repository...")
+        webbrowser.open("https://github.com/Notxnorand73/NXA-Developer-Portal")
+    elif option == "3":
+        print("Exiting...")
+        quit()
+    else:
+        print("Invalid option. Please try again.")
